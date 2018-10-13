45fcf13219a9352170ca63f97c43dfd6ef6df00e
Jimin Hong <jhong.dev@gmail.com>
2018-10-13 17:42:50 -0400

    created posts folder, post-create folder, made class called AppPostCreate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { enableProdMode } from '@angular/core';
+
+enableProdMode();
 
 @NgModule({
+  //now angular know AppComponents
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,15 @@
+
+//3. need to import the decorator, Component
+import { Component } from '@angular/core';
+
+//2. need a decorator so angular knows the class PostCreateComponent
+@Component({
+    //name starts with app- so no confusion with html
+    selector: 'app-post-create',
+    templateUrl: './post-create.component.html'
+})
+
+//1. creating component by making a class
+export class PostCreateComponent {
+
+}
